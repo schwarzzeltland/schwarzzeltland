aa0ea1347ea7c0622f35a1b55923a1beb4f1bd13
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2024-05-26 01:55:29 +0200

    Don't need config.py

--- schwarzzeltland/settings.py
+++ schwarzzeltland/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
-import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", config.SECRET_KEY)
+SECRET_KEY = "django-insecure--al=tp&7a5-h=x0gb8)$r&%pdo$=u&lonrtol7tbatc^%!8!ci"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if "DJANGO_NO_DEBUG" in os.environ else True
