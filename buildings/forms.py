ac4bb77d3d670658bbe768ccaa0f528d0acdba38
Sascha Cuppenbender <sascha-cuppenbender@t-online.de>
2024-05-26 19:50:10 +0200

    Constructions anzeigen und hinzufügen

--- buildings/forms.py
+++ buildings/forms.py
@@ -1,9 +1,12 @@
+from typing import Type
+
 from django.db.models import Q
 from urllib3.filepost import iter_field_objects
 
-from buildings.models import Material, StockMaterial, Construction
+from buildings.models import Material, StockMaterial, Construction, ConstructionMaterial
 
-from django.forms import IntegerField, CharField, ModelForm, Form, ModelChoiceField
+from django.forms import IntegerField, CharField, ModelForm, Form, ModelChoiceField, inlineformset_factory, \
+    BaseModelFormSet, BaseModelForm
 
 
 class AddMaterialStockForm(ModelForm):
@@ -39,7 +42,6 @@ class MaterialForm(ModelForm):
 
 
 class ConstructionForm(ModelForm):
-
     def __init__(self, *args, **kwargs):
         organization = kwargs.pop('organization', None)
         super(ConstructionForm, self).__init__(*args, **kwargs)
@@ -50,5 +52,29 @@ class ConstructionForm(ModelForm):
         fields = '__all__'
         exclude = ['owner']
 
+
 class ImportConstructionForm(Form):
-    construction = ModelChoiceField(queryset=Construction.objects.filter(owner__isnull=True))
+    construction = ModelChoiceField(queryset=Construction.objects.none())
+
+    def __init__(self, *args, **kwargs):
+        organization = kwargs.pop('organization', None)
+        super(ImportConstructionForm, self).__init__(*args, **kwargs)
+        self.organization = organization
+        self.fields['construction'].queryset = Construction.objects.filter(Q(owner=organization) | Q(owner__isnull=True) | Q(public=True))
+
+class ConstructionMaterialForm(ModelForm):
+    count = IntegerField(required=True)
+    storage_place = CharField(required=False)
+    def __init__(self, *args, **kwargs):
+        organization = kwargs.pop('organization', None)
+        super(ConstructionMaterialForm, self).__init__(*args, **kwargs)
+        self.instance.organization = organization
+
+    class Meta:
+        model = ConstructionMaterial
+        fields = '__all__'
+        exclude = ['construction']
+
+ConstructionMaterialFormSet = inlineformset_factory(
+    Construction, ConstructionMaterial, form=ConstructionMaterialForm, extra=15
+)
--- buildings/views.py
+++ buildings/views.py
@@ -1,24 +1,39 @@
 from django.contrib import messages
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
+from setuptools._distutils.command.config import config
 from urllib3 import request
 
-from buildings.forms import AddMaterialStockForm, MaterialForm, ConstructionForm,ImportConstructionForm
-from buildings.models import StockMaterial, Construction
+from buildings.forms import AddMaterialStockForm, MaterialForm, ConstructionForm, ImportConstructionForm, \
+    ConstructionMaterialForm, ConstructionMaterialFormSet
+from buildings.models import StockMaterial, Construction, ConstructionMaterial
 from main.models import Membership
 
 
 def constructions(request):
     m: Membership = request.user.membership_set.filter(organization=request.org).first()
     if m.material_manager:
-        form = ImportConstructionForm()
+        form = ImportConstructionForm(organization=request.org)
         if request.method == 'POST':
-             form = ImportConstructionForm(request.POST)
-             if form.is_valid():
-                c: Construction=form.cleaned_data["construction"]
-                c.pk=None
-                c.owner=request.org
+            form = ImportConstructionForm(request.POST, organization=request.org)
+            if form.is_valid():
+                c: Construction = form.cleaned_data["construction"]
+                # Extrahieren Sie den ersten Teil des Namens (bis zum ersten Leerzeichen)
+                first_part = c.name.split(' ', 1)[0]
+                # Überprüfen, ob ein Objekt mit dem ersten Teil des Namens bereits existiert
+                existing_constructions = Construction.objects.filter(owner=request.org, name__startswith=first_part)
+                if existing_constructions.exists():
+                    # Wenn ein Objekt mit diesem Namen gefunden wird, zählen Sie die Anzahl
+                    existing_count = existing_constructions.count()
+                    new_name = f"{first_part} {existing_count + 1}"
+                else:
+                    # Andernfalls verwenden Sie den Namen wie gewohnt
+                    new_name = c.name + ' 1'
+                c.pk = None
+                c.owner = request.org
+                c.name = new_name
+                c.public = False
                 c.save()
                 messages.success(request, 'Konstruktion hinzugefügt')
                 return HttpResponseRedirect(reverse_lazy('constructions'))
@@ -32,17 +47,27 @@ def constructions(request):
 
 
 def edit_construction(request):
-    form = ConstructionForm(organization=request.org)
     if request.method == 'POST':
-        form = ConstructionForm(request.POST, organization=request.org)
-        if form.is_valid():
-            Construction.objects.create(name=form.instance, owner=request.org)
-            messages.success(request, 'Konstruktion hinzugefügt')
-            return HttpResponseRedirect(reverse_lazy('constructions'))
+        construction_form = ConstructionForm(request.POST)
+        material_formset = ConstructionMaterialFormSet(request.POST)
 
+        if construction_form.is_valid() and material_formset.is_valid():
+            construction = construction_form.save(commit=False)
+            construction.owner = request.org
+            construction.save()
+            messages.success(request, 'Konstruktion hinzugefügt')
+            materials = material_formset.save(commit=False)
+            for material in materials:
+                material.construction = construction
+                material.save()
+            return HttpResponseRedirect(reverse_lazy('constructions'))  # Beispiel-URL für die Weiterleitung
+    else:
+        construction_form = ConstructionForm()
+        material_formset = ConstructionMaterialFormSet()
     return render(request, 'buildings/edit_constructions.html', {
-        'title': 'Konstruktion hinzufügen',
-        'form': form,
+        'title' : 'Konstruktion hinzufügen',
+        'construction_form': construction_form,
+        'material_formset': material_formset,
     })
 
 
@@ -79,6 +104,6 @@ def edit_material(request):
             return HttpResponseRedirect(reverse_lazy('material'))
 
     return render(request, 'buildings/edit_material.html', {
-        'title': 'Add Material',
+        'title': 'Material hinzufügen',
         'form': form,
     })
