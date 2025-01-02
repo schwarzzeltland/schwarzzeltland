d391e6a6b7ddd017af6853029d0cb6b09b7fbc8d
Sascha Cuppenbender <sascha-cuppenbender@t-online.de>
2025-01-02 15:37:34 +0100

    Events: Locations und Trips

--- events/urls.py
+++ events/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('main', views.events, name='events'),
+    path('trip', views.trip, name='trip'),
+    path('location', views.location, name='location'),
 
 ]
--- events/views.py
+++ events/views.py
@@ -3,7 +3,13 @@ from django.shortcuts import render
 
 
 @login_required
-def events(request):
-    return render(request, 'events/events.html', {
+def trip(request):
+    return render(request, 'events/trip.html', {
         'title': 'Veranstaltungen',
+    })
+
+@login_required
+def location(request):
+    return render(request, 'events/locations.html', {
+        'title': 'Orte',
     })
