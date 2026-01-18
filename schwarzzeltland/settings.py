9075b5e9da33b3897cba495167c7607f38e65e80
Sascha Cuppenbender <sascha-cuppenbender@t-online.de>
2026-01-18 21:35:05 +0100

    To-DO Email bei Fälligkeit

--- schwarzzeltland/settings.py
+++ schwarzzeltland/settings.py
@@ -40,7 +40,7 @@ CELERY_BEAT_SCHEDULE = {
     },
     "send_due_checklist_items_daily": {
         "task": "main.tasks.send_due_checklist_items_today",
-        "schedule": crontab(hour=1, minute=00)  # 08:00 Uhr
+        "schedule": crontab(hour=1, minute=0)  # 08:00 Uhr
     },
 
 }
